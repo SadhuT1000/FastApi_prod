82821074e984d4c887acb418feb9bfdb5913f6d5
maksbolomoznov <bolomozhnov@gmail.com>
2025-05-09 16:56:04 +0700

    all crud

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
+from contextlib import asynccontextmanager
 from sys import prefix
+from xml.etree.ElementInclude import include
+
 from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
 import uvicorn
-
+from core.models import Base, db_helper
 from items_views import router as items_router
 from users.views import router as user_router
+from api_v1 import router as router_v1
+from core.config import settings
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 app.include_router(items_router)
 app.include_router(user_router)
 
